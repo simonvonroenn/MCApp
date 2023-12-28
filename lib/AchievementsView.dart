638e2e88629b31eefd5fc6d31bd73fe57057def6
simon.vonroenn <simon.vonroenn@web.de>
2023-12-28 17:37:03 +0100

    Add banners to cookie shop and achievements

--- lib/AchievementsView.dart
+++ lib/AchievementsView.dart
@@ -33,7 +33,7 @@ class _AchievementViewState extends State<AchievementsView> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     return Container(
-      height: MediaQuery.of(context).size.height * 0.4,
+      height: MediaQuery.of(context).size.height * 0.5,
       decoration: BoxDecoration(
         color: Colors.white.withOpacity(0.7),
         borderRadius: const BorderRadius.only(
@@ -41,46 +41,72 @@ class _AchievementViewState extends State<AchievementsView> {
           topRight: Radius.circular(20),
         ),
       ),
-      child: ListView.builder(
-        itemCount: widget.achievementsNotifier.value.length,
-        itemBuilder: (context, index) {
-          Achievement achievement = widget.achievementsNotifier.value[index];
-          bool isFulfilled = achievement.fulfilled;
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Checkbox Column
-                Container(
-                  width: screenWidth * 0.25,
-                  alignment: Alignment.center,
-                  child: Icon(
-                      isFulfilled ? Icons.check_circle : Icons.circle_rounded,
-                    color: isFulfilled ? Colors.green : Colors.grey,
-                    size: 50,
-                  )
-                ),
-                // Name and Description Column
-                Container(
-                  width: screenWidth * 0.75,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.grey.withOpacity(0.7),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
+            child: const Text(
+              'Achievements',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.achievementsNotifier.value.length,
+              itemBuilder: (context, index) {
+                Achievement achievement = widget.achievementsNotifier.value[index];
+                bool isFulfilled = achievement.fulfilled;
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
-                        achievement.name,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16
+                      // Checkbox Column
+                      Container(
+                          width: screenWidth * 0.25,
+                          alignment: Alignment.center,
+                          child: Icon(
+                            isFulfilled ? Icons.check_circle : Icons.circle_rounded,
+                            color: isFulfilled ? Colors.green : Colors.grey,
+                            size: 70,
+                          )
+                      ),
+                      // Name and Description Column
+                      Container(
+                        width: screenWidth * 0.75,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              achievement.name,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16
+                              ),
+                            ),
+                            Text(achievement.description),
+                          ],
                         ),
                       ),
-                      Text(achievement.description),
                     ],
                   ),
-                ),
-              ],
+                );
+              },
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
--- lib/CookieShop.dart
+++ lib/CookieShop.dart
@@ -38,10 +38,7 @@ class _CookieShopState extends State<CookieShop> {
         .size
         .width;
     return Container(
-      height: MediaQuery
-          .of(context)
-          .size
-          .height * 0.4,
+      height: MediaQuery.of(context).size.height * 0.5,
       decoration: BoxDecoration(
         color: Colors.white.withOpacity(0.7), // Semi-transparent white
         borderRadius: const BorderRadius.only(
@@ -49,63 +46,89 @@ class _CookieShopState extends State<CookieShop> {
           topRight: Radius.circular(20),
         ),
       ),
-      child: ListView.builder(
-        key: UniqueKey(),
-        itemCount: widget.autoClickersNotifier.value.length,
-        itemBuilder: (context, index) {
-          AutoClicker autoClicker = widget.autoClickersNotifier.value[index];
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5),
-            child: InkWell(
-                onTap: () => widget.onBuy(autoClicker),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // First Column: Icon
-                    Container(
-                      width: screenWidth * 0.25,
-                      alignment: Alignment.center,
-                      child: Image.asset(
-                          autoClicker.iconPath, width: 80, height: 80),
-                    ),
-                    // Second Column: Name and Price
-                    Container(
-                      width: screenWidth * 0.50,
-                      alignment: Alignment.center,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            autoClicker.name,
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                          Text(
-                            '${autoClicker.price} Cookies',
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
-                      ),
-                    ),
-                    // Third Column: Level and CPS
-                    Container(
-                      width: screenWidth * 0.25,
-                      alignment: Alignment.center,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Level: ${autoClicker.level}'),
-                          Text('CPS: ${autoClicker.cps}'),
-                        ],
-                      ),
-                    ),
-                  ],
-                )
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.grey.withOpacity(0.7),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
             ),
-          );
-        },
+            child: const Text(
+              'Power Ups',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              key: UniqueKey(),
+              itemCount: widget.autoClickersNotifier.value.length,
+              itemBuilder: (context, index) {
+                AutoClicker autoClicker = widget.autoClickersNotifier.value[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: InkWell(
+                      onTap: () => widget.onBuy(autoClicker),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // First Column: Icon
+                          Container(
+                            width: screenWidth * 0.25,
+                            alignment: Alignment.center,
+                            child: Image.asset(
+                                autoClicker.iconPath, width: 70, height: 70),
+                          ),
+                          // Second Column: Name and Price
+                          Container(
+                            width: screenWidth * 0.50,
+                            alignment: Alignment.center,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  autoClicker.name,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold, fontSize: 16
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '${autoClicker.price} Cookies',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            ),
+                          ),
+                          // Third Column: Level and CPS
+                          Container(
+                            width: screenWidth * 0.25,
+                            alignment: Alignment.center,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Level: ${autoClicker.level}'),
+                                Text('CPS: ${autoClicker.cps}'),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
